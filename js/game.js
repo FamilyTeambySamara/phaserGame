e12737dc400557a19ad11d98f7daf8bc311157d2
IlayDeveloper <32011084+IlayDeveloper@users.noreply.github.com>
2017-11-26 00:02:43 +0400

    1

--- js/game.js
+++ js/game.js
@@ -1,15 +1,10 @@
-// var game = new Phaser.Game(480, 640, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-//
-
-
-
 (function () {
+
+//Спрайты================
 var playerBet;
 var computerBet;
 var computerBet_2;
-
-var ball;
-
+//======================
 
 ///Таблица данных об игре
 var scoreTable;
@@ -17,59 +12,41 @@ var levelTable;
 var healthTable;
 //=============================
 
-var ballReleased = false;
-
-var computerBetSpeed = 150;
-var ballSpeed = 300;
-var score = 0;
-
-var nextLevelScore = 5;
-var Level = 1;
-
-var health = 3;
-
-
-// var playerPosition = 0;
-// var compPosition = 1;
-
-
-
+//============================Переменные значения(настройки игры)==================
+var computerBetSpeed = 150; //Скорость передвижения компьютера
+var score = 0; //Количество попаданий
+var nextLevelScore = 5; //Критерий перехода на следующий уровень
+var Level = 1; //Текущий уровень
+var health = 3; //Жизни игрока
+//==================================================================
+//===========Управление============================================
+// var plyaerMouse;
 var fireButton;
+//=====Для стрельбы=================================================
 
 
-
-
-
-
+//Группы пуль
 var bullets;
-var bullet;
-var trigerBullet = false;
-
-//enemys
-
-var bulletTime = 0;
-var enemyBulletTime = 0;
-var enemyBulletvelocity = 400;
-var timeDilay = 1000;
-
-
-var bigemnemys;
-var bigenemy;
-var emnemysBig;
-
 var enemyBullets;
+var enemyBulletsBig;
+//Экземплры пуль
+var bullet;
 var enemyBullet;
-
 var enemyBulletBig;
 
-var  enemyBullestBig;
+//Параметры стрельбы
+var bulletTime = 1000;  //Начальная задержка пуль по времени PLayer
+var enemyBulletTime = 1000;//Начальная задержка пуль по времени Enemys
 
+var enemyBulletvelocity = 400;//скорость пуль PLayer
+var playerBulletVelocity = 400;//скорость пуль Enemys
 
-
-
-
-
- var Game =
+var timeDilayEnemy = 1000;//перезардка выстрела  PLayer
+var timeDilayPlayer = 600;//перезардка выстрела  Enemys
+//=================Результат матча========================================
+var saveBox = {};
+/////////================================================\\\\\\\\\\\\\\\\\\\\
+window.Game =
 {
     preload: function () {
         game.load.image('bet', 'assets/img/bet_3.png');
@@ -80,12 +57,8 @@ var  enemyBullestBig;
 
     create: function () {
 
-
-      game.physics.startSystem(Phaser.Physics.ARCADE);
-
-
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     //  game.add.tileSprite(0, 0, 800, 600, 'background');
-
 
 //=============Снег на фоне игры===================================
       back_emitter = this.add.emitter(game.world.centerX, -32, 600);
@@ -97,7 +70,6 @@ var  enemyBullestBig;
       back_emitter.width = game.world.width * 1.5;
       back_emitter.minRotation = 0;
       back_emitter.maxRotation = 40;
-
       back_emitter.start(false, 14000, 20);
 //=======Пули игрока============================
       bullets = game.add.group();
@@ -108,7 +80,7 @@ var  enemyBullestBig;
       bullets.setAll('anchor.y', 1);
       bullets.setAll('outOfBoundsKill', true);
       bullets.setAll('checkWorldBounds', true);
-//============Пули противника=======================
+//============Пули противника обыкновенные=======================
       enemyBullets = game.add.group();
       enemyBullets.enableBody = true;
       enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
@@ -128,107 +100,26 @@ var  enemyBullestBig;
       enemyBulletsBig.setAll('anchor.y', 1);
       enemyBulletsBig.setAll('outOfBoundsKill', true);
       enemyBulletsBig.setAll('checkWorldBounds', true);
-//============================================
+//====================Создаем спрайты игроков===
 
+      computerBet = this.createBet(game.world.centerX, 450);
+      playerBet = this.createBet(game.world.centerX, 20);
 
-          computerBet_2 = this.createBet(game.world.centerX, 600);
-          computerBet_2.scale.setTo(1.5, 1.5);
-          //computerBet_2.body.collideWorldBounds = true;
-
-
-//=================Big Enemy============================
-
-      // emnemysBig = game.add.group();
-      // emnemysBig.enableBody = true;
-      // emnemysBig.physicsBodyType = Phaser.Physics.ARCADE;
-      // emnemysBig.createMultiple(1, 'bet');
-      // emnemysBig.setAll('anchor.x', 0.5);
-      // emnemysBig.setAll('anchor.y', 1);
-      // emnemysBig.setAll('outOfBoundsKill', true);
-      // emnemysBig.setAll('checkWorldBounds', true);
-    //  emnemysBigBullets.scale.setTo(2, 2);
-      // emnemysBig.setAll('outOfBoundsKill', true);
-      // emnemysBig.setAll('checkWorldBounds', true);
-
-//=====================================
-      // function bulletGo ()
-      // {
-      //   if (trigerBullet){
-      //
-      //     bullet = bullets.getFirstExists(false);
-      //     bullet.reset(playerBet.x, playerBet.y + 25);
-      //     // bullet.x = playerBet.x;
-      //     // bullet.y = playerBet.y + 10;
-      //     bullet.body.velocity.y = 50;
-      //   }
-      // }
-      //
-      // setInterval(bulletGo , 2000);
-
-//======================================================
-
+      computerBet_2 = this.createBet(game.world.centerX, 600);
+      computerBet_2.scale.setTo(1.5, 1.5);
 
       scoreTable =  game.add.text(16, 60, 'score: ' + score, { fontSize: '32px', fill: 'red' });
       levelTable = game.add.text(16, 16, 'Level: ' + Level, { fontSize: '32px', fill: 'white' });
       healthTable = game.add.text(16, 95, 'Health: ' +  health, { fontSize: '32px', fill: 'blue' });
 
-
-//==========Создаем игроков================================
-      computerBet = this.createBet(game.world.centerX, 450);
-      playerBet = this.createBet(game.world.centerX, 20);
-
-
-//==========================================================
-
-       //  weapon = game.add.weapon(1, 'ball');
-       // weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-       // weapon.bulletAngleOffset = 90;
-       // weapon.bulletSpeed = -400;
-       // weapon.trackSprite(playerBet, 14, 0);
-
-
-
 //=========Управление игроком========================================
        fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+       // plyaerMouse = game.input.x;
        //game.input.onDown.add(this.releaseBall, this);
 //===================================================================
 
-       // setInterval(createBall, 300);
-       //
-       // setInterval(createEnemyBall, 200);
-
-    //  game.add.button( 15, 50, 'button_over', this.goMenu, this);
-      // ball = game.add.sprite(-25, playerBet.y + 10, 'ball');
-      //
-      //   function createBall () {
-      //       if (triger) {
-      //           triger = false;
-      //           ball = game.add.sprite(playerBet.x, playerBet.y + 10, 'ball');
-      //           // ball.x = playerBet.x;
-      //           // ball.y = playerBet.y + 10;
-      //           game.physics.arcade.enable(ball);
-      //           //ball.body.collideWorldBounds = true;
-      //         //  ball.body.bounce.setTo(1, 1);
-      //           ball.body.velocity.y = 400;
-      //       }
-      //   }
-      //
-      //   enemyBall = game.add.sprite(-25, playerBet.y + 10, 'ball');
-
-        // function createEnemyBall () {
-        //     if (enemyTriger) {
-        //         enemyTriger = false;
-        //         enemyBall = game.add.sprite(computerBet.x, computerBet.y - 10, 'ball');
-        //         // ball.x = playerBet.x;
-        //         // ball.y = playerBet.y + 10;
-        //         game.physics.arcade.enable(enemyBall);
-        //         //ball.body.collideWorldBounds = true;
-        //       //  ball.body.bounce.setTo(1, 1);
-        //         enemyBall.body.velocity.y = -400;
-        //     }
-        // }
     },
-
+    //Создание спрайтов
     createBet: function (x, y) {
         var bet = game.add.sprite(x, y, 'bet');
         bet.anchor.setTo(0.5, 0.5);
@@ -238,7 +129,7 @@ var  enemyBullestBig;
       //  bet.body.immovable = true;
         return bet;
     },
-
+    //Стрельба PLayer
      fireBullet: function () {
 
     //  To avoid them being allowed to fire too fast we set a time limit
@@ -251,13 +142,13 @@ var  enemyBullestBig;
         {
             //  And fire it
             bullet.reset(playerBet.x, playerBet.y + 8);
-            bullet.body.velocity.y = 400;
-            bulletTime = game.time.now + 350;
+            bullet.body.velocity.y = playerBulletVelocity;
+            bulletTime = game.time.now + timeDilayPlayer;
         }
     }
 
 },
-
+    //Стрельба Enemy
     fireBulletEnemy: function () {
 
       if (game.time.now > enemyBulletTime)
@@ -265,52 +156,15 @@ var  enemyBullestBig;
           //  Grab the first bullet we can from the pool
           enemyBullet = enemyBullets.getFirstExists(false);
 
-          //enemyBullet.scale.setTo(2,2);
-          //moon.scale.setTo(0.5, 0.5);
           if (enemyBullet)
           {
               //  And fire it
               enemyBullet.reset(computerBet.x, computerBet.y - 8);
               enemyBullet.body.velocity.y = -enemyBulletvelocity;
-              enemyBulletTime = game.time.now + timeDilay;
+              enemyBulletTime = game.time.now + timeDilayEnemy;
           }
       }
     },
-
-
-
-    // goOverMenu: function ()
-    // {
-    //     this.state.start('Game_over');
-    // },
-
-    // releaseBall: function () {
-    //   // if (!ballReleased) {
-    //   //   ball.body.velocity.x = ballSpeed;
-    //   //   ball.body.velocity.y = -ballSpeed;
-    //   //   ballReleased = true;
-    //   // }
-    // },
-///пересечение со снарядом==================================================================
-    // ballHitsBet: function (_ball, _bet) {
-    //      var diff = 0;
-    //
-    //      if (_ball.x < _bet.x) {
-    //          //  Шарик находится с левой стороны ракетки
-    //          diff = _bet.x - _ball.x;
-    //          _ball.body.velocity.x = (-10 * diff);
-    //      }
-    //      else if (_ball.x > _bet.x) {
-    //          //  Шарик находится с правой стороны ракетки
-    //          diff = _ball.x -_bet.x;
-    //          _ball.body.velocity.x = (10 * diff);
-    //      }
-    //      else {
-    //          //  Шарик попал в центр ракетки, добавляем немножко трагической случайности его движению
-    //          _ball.body.velocity.x = 2 + Math.random() * 8;
-    //      }
-    //  },
-
 
      killComp: function (bet, bullet){
 
@@ -322,34 +176,16 @@ var  enemyBullestBig;
 
                 nextLevelScore += 5;
                 enemyBulletvelocity += 70;
-                timeDilay -= 50;
+                timeDilayEnemy -= 50;
                 ++Level;
                 levelTable.text = "Level" + Level;
                 //game.state.start('nextLevel', false);
             }
      },
-///////////================================================================================
-     // checkBall: function () {
-     //     if (ball.y < 10) {
-     //        ball.kill();
-     //     } else if (ball.y > 450) {
-     //          ball.kill();
-     //    }
-     //
-     //  },
-     // setBall: function () {
-     //     if (ballReleased) {
-     //        ball.x = game.world.centerX;
-     //        ball.y = game.world.centerY;
-     //        ball.body.velocity.x = 0;
-     //        ball.body.velocity.y = 0;
-     //        ballReleased = false;
-     //     }
-     // },
-     // SnowBallOver: function ()
-     // {
-     //
-     // },
+
+     getInfo: function (){
+            return saveBox.score;
+     },
 
      killPLayer: function (bet, bullet )
      {
@@ -357,20 +193,28 @@ var  enemyBullestBig;
         health = health - 1;
         if (health < 1)
         {
-           this.state.start('Game_over');
-           health = 3;
-           shotAim = 0;
+            saveBox = {score: score};
+            //========Сброс настроек====
+            nextLevelScore = 5;
+            health = 3;
+            shotAim = 0;
+            Level = 1;
+            score = 0;
+            timeDilayEnemy = 1000;
+            enemyBulletvelocity = 400;
+            //==============================
+            this.state.start('Game_over');
         }
         healthTable.text = 'HitPoint: ' + health;
      },
 
      update: function () {
-
+       //Если нажат пробел запускаем стрельбу PLayer
         if (fireButton.isDown)
         {
             this.fireBullet();
         }
-
+        //Если комп рядом с Player - запускаем стрельбу
         if ( Math.abs(playerBet.x - computerBet.x) < 35 ){
 
             this.fireBulletEnemy();
@@ -398,22 +242,6 @@ var  enemyBullestBig;
             computerBet.body.velocity.x = 0;
          }
 //==================Управление большим врагом=============================
-          // var i = 0;
-          //
-          // if (Level == 2 && i == 0){
-          //   i++;
-          //     //enemyBullets.getFirstExists(false)
-          //   computerBet_2 = emnemysBig.getFirstExists(false);
-          //
-          //   if (computerBet_2)
-          //   {
-          //       //  And fire it
-          //       computerBet_2.reset(50, 250);
-          //       computerBet_2.body.velocity.x = (computerBetSpeed/2);
-          //   }
-          //
-          // }
-
          if(Level >= 2 ){
 
               if (computerBet_2.y > 400){
@@ -421,7 +249,6 @@ var  enemyBullestBig;
               }else if (computerBet_2.y <= 400){
                   computerBet_2.body.velocity.y = 0;
               }
-
 
               if(playerBet.x > 400 && !(computerBet_2.x > 800)){
                   computerBet_2.body.velocity.x = computerBetSpeed/2;
@@ -437,24 +264,21 @@ var  enemyBullestBig;
 //==========================================================================
          // game.physics.arcade.collide(ball, playerBet);
          //game.physics.arcade.collide(ball, computerBet);
-
-         game.physics.arcade.overlap(bullets, computerBet, this.killComp, null, this);
-         game.physics.arcade.overlap(enemyBullets, playerBet, this.killPLayer, null, this);
-
+//Реакция на пересечение пуль и игроков================================================
+              game.physics.arcade.overlap(bullets, computerBet, this.killComp, null, this);
+              game.physics.arcade.overlap(bullets, computerBet_2, this.killComp, null, this);
+              game.physics.arcade.overlap(enemyBullets, playerBet, this.killPLayer, null, this);
+              game.physics.arcade.overlap(enemyBulletsBig, playerBet, this.killPLayer, null, this);
+//======================================================================================
          // game.physics.arcade.collide(ball, playerBet, this.boomBol(playerBet), null, this);
          // game.physics.arcade.collide(ball, computerBet, this.boomBol(computerBet), null, this);
         // this.goSnowBall();
          //this.checkBall();
-     }
-
-
-
+     },
 
 
 }
-
-window.Game = Game;
-
+///===Конец объекта Game=====================================================================================
 
  function fireBulletEnemyBig () {
 
@@ -462,15 +286,14 @@ window.Game = Game;
   {
       //  Grab the first bullet we can from the pool
       enemyBulletBig = enemyBulletsBig.getFirstExists(false);
-
       enemyBulletBig.scale.setTo(3,3);
-      //moon.scale.setTo(0.5, 0.5);
+
       if (enemyBulletBig)
       {
           //  And fire it
           enemyBulletBig.reset(computerBet_2.x, computerBet_2.y - 15);
           enemyBulletBig.body.velocity.y = -enemyBulletvelocity/2;
-          enemyBulletTime = game.time.now + timeDilay;
+          enemyBulletTime = game.time.now + timeDilayEnemy;
       }
   }
 }
--- js/game_over.js
+++ js/game_over.js
@@ -1,10 +1,10 @@
 var Game_over =
 {
-
     create: function ()
     {
       this.add.tileSprite(0, 0, 640, 450, 'Game_over');
       this.add.text(250, 40, 'GAME OVER', { fontSize: '32px', fill: 'red' });
+      this.add.text(250, 85, 'SCORE ' + Game.getInfo(), { fontSize: '32px', fill: 'red' });
       this.add.button( 250, 250, 'button', this.startPlay, this, 2, 1, 0);
       this.add.button( 250, 150, 'button', this.goToMenu, this, 2, 1, 0);
     },
